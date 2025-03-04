f46330262b2cc9536e2877a7c452c6997a268b21
Fahad-1214 <fahadwork674@gmail.com>
2025-03-04 10:39:11 +0500

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import image from "../../public/Homepage.jpg";
 
 export default function Home() {
   return (
-    <div className="h-[700px] flex justify-center items-center">
-      Home page
+    <div className="relative h-screen w-screen">
+      <Image
+        src={image}
+        fill
+        alt="Homepage Background"
+        style={{ objectFit: "cover" }}
+      />
+      <div className="absolute inset-0 flex justify-center items-center">
+        {/* Your content goes here */}
+      </div>
     </div>
   );
 }
